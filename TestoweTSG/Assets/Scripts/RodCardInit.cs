e559aa6e412f1c26b1f331fc51cda00307a28c5b
Mikolaj <mikolajb398@gmail.com>
2019-05-01 00:45:32 +0200

    Added script that initialize random values for rod cards and script for rod card button that changes the rod 3D model

---
+++ TestoweTSG/Assets/Scripts/RodCardInit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI
+    ;
+public class RodCardInit : MonoBehaviour
+{
+    public int progressBarIndex=3;
+    GameObject progressBar;
+    int nextLevelPoints;
+    int gainedPoints;
+
+    void Start()
+    {
+        progressBar = transform.GetChild(progressBarIndex).gameObject;
+        InitLevelPointsValues();
+        SetText();
+        InitSlider();
+        CheckUpgradeArrow();
+    }
+
+    void InitLevelPointsValues()
+    {
+        nextLevelPoints = Random.Range(1, 20);
+        gainedPoints = Random.Range(1, 40);
+    }
+    void InitSlider()
+    {
+        Slider slider = progressBar.GetComponentInChildren<Slider>();
+        slider.maxValue = nextLevelPoints;
+        slider.value = gainedPoints;
+    }
+    void SetText()
+    {
+        Text progressText = progressBar.GetComponentInChildren<Text>();
+        progressText.text = gainedPoints+"/"+nextLevelPoints;
+        Debug.Log(progressText.gameObject.name);
+    }
+    void CheckUpgradeArrow()
+    {
+        Button upgradeArrow = progressBar.GetComponentInChildren<Button>();
+        if (gainedPoints>=nextLevelPoints) upgradeArrow.gameObject.SetActive(true);
+        else upgradeArrow.gameObject.SetActive(false);
+
+    }
+
+}
---
+++ TestoweTSG/Assets/Scripts/RodCardPressed.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class RodCardPressed : MonoBehaviour
+{
+    public GameObject rod;
+    private Button button;
+    private bool choosen;
+    private Transform context;
+    private GameObject selectedText;
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(ButtonPressed);
+        rod.tag = "Rod";
+        choosen = false;
+        FindGameObjects();
+    }
+    void FindGameObjects()
+    {
+        context = GameObject.FindGameObjectWithTag("Context").transform;
+        selectedText = gameObject.transform.parent.GetChild(transform.GetSiblingIndex() - 1).gameObject;
+    }
+    public void ButtonPressed()
+    {
+        if (choosen) return;
+        ChangeRod();
+        selectedText.SetActive(true);
+        choosen = true;
+    }
+    void ChangeRod()
+    {
+        Transform oldRod = GameObject.FindGameObjectWithTag("Rod").gameObject.transform;
+        rod = Instantiate(rod, oldRod.position, oldRod.rotation).gameObject;
+        rod.transform.SetParent(context);
+        rod.transform.localScale = new Vector3(300, 300, 300);
+        rod.name = "RodModel";
+        Destroy(oldRod.gameObject);
+    }
+    public void ChangeChoose()
+    {
+        choosen = false;
+    }
+}
